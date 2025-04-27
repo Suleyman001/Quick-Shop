24364df3e61dcc8037cd8eecb8333a57a69da44a
melikYalcinkaya <melikyalcinkaya55@gmail.com>
2025-04-27 18:17:46 +0300

    created  the login page ui

--- app/src/main/java/nje/hu/quickshop/MainActivity.java
+++ app/src/main/java/nje/hu/quickshop/MainActivity.java
@@ -1,9 +1,12 @@
 package nje.hu.quickshop;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -20,5 +23,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Status bar color. (somehow couldn't do it in themes.xml so i just code it)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.my_status_bar_color));
+        }
+
     }
 }
--- app/src/main/java/nje/hu/quickshop/ui/registration/RegistrationActivity.java
+++ app/src/main/java/nje/hu/quickshop/ui/registration/RegistrationActivity.java
@@ -1,4 +1,15 @@
 package nje.hu.quickshop.ui.registration;
 
-public class RegistrationActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import nje.hu.quickshop.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration); // XML ile bağlantı
+    }
 }
