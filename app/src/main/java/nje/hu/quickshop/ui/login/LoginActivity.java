c7b590bf53ecb072f74faf9bbeda05bd34b81c4c
Marguerite Anne <margaridacv81@hotmail.com>
2025-04-29 20:36:11 +0200

    Enable navigation from login screen to registration via TextView click

--- app/src/main/java/nje/hu/quickshop/ui/login/LoginActivity.java
+++ app/src/main/java/nje/hu/quickshop/ui/login/LoginActivity.java
@@ -1,13 +1,18 @@
 package nje.hu.quickshop.ui.login;
 
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import nje.hu.quickshop.R;
+import nje.hu.quickshop.ui.registration.RegistrationActivity;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -22,5 +27,14 @@ public class LoginActivity extends AppCompatActivity {
             Window window = getWindow();
             window.setStatusBarColor(ContextCompat.getColor(this, R.color.my_status_bar_color));
         }
+
+        TextView newAccountText = findViewById(R.id.login_newAccountText);
+        newAccountText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
